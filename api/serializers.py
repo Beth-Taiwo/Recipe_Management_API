19378d052bd1bc2cff51a64a0161583be3dc317e
Beth Taiwo <popoolataiwo22@gmail.com>
2025-01-03 16:57:08 +0000

    Ensure that username and email is unique on creation

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email','password']
+        
+    def validate(self, obj):
+        if User.objects.filter(username=obj['username']).exists():
+            raise serializers.ValidationError("Username already exists.")
+        if User.objects.filter(email=obj['email']).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return obj
