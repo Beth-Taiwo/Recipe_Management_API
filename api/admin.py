a2efd65d5200675cb3ad82d877a89b8491a0a7b6
Beth Taiwo <popoolataiwo22@gmail.com>
2025-01-10 13:25:38 +0000

    registered models in admin view

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient, Category, Course
 
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(Ingredient)
+admin.site.register(Category)
+admin.site.register(Course)
